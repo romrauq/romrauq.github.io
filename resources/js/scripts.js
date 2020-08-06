7209cfacfc33864d02ebd709c0381b78c4e74e6e
John Quarmor <johnequarmor@gmail.com>
2020-08-06 14:54:37 +0000

    Added side menu button functionality in JS file.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,9 @@
-function openSlideMenu() {}
+function openSlideMenu() {
+	document.getElementById("side-menu").style.width = "70vw";
+	document.getElementById("showcase-body").style.pointerEvents = "none";
+}
 
-function closeSlideMenu() {}
+function closeSlideMenu() {
+	document.getElementById("side-menu").style.width = "0";
+	document.getElementById("showcase-body").style.pointerEvents = "all";
+}
