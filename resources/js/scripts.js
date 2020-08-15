ec8cd25c8b6b966cdc3583c1e5872c3d2d5f6d00
John Quarmor <johnequarmor@gmail.com>
2020-08-15 20:50:09 +0000

    Minor edit to JS file.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -5,12 +5,10 @@ closeButton.addEventListener("click", closeSlideMenu);
 
 function openSlideMenu() {
 	document.getElementById("side-menu").style.width = "100vw";
-	document.getElementById("showcase-body").style.pointerEvents = "none";
 	burgerButton.style.display = "none";
 }
 
 function closeSlideMenu() {
 	document.getElementById("side-menu").style.width = "0";
-	document.getElementById("showcase-body").style.pointerEvents = "all";
 	burgerButton.style.display = "block";
 }
